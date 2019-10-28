994080f569ff30f13103eea4fdb6666b74af6c22
kdglider <kevindong80@gmail.com>
2019-10-28 08:37:20 -0400

    Wrote a simple subscriber 'listener' and modified CMakeLists based on the tutorial

---
+++ src/listener.cpp
@@ -0,0 +1,30 @@
+/**Copyright (c) 2019   Hao Da (Kevin) Dong
+ * @file       listener.cpp
+ * @date       10/27/2019
+ * @brief      Simple ROS subscriber from the beginner tutorials
+ * @license    This project is released under the BSD-3-Clause License.
+ */
+
+#include "ros/ros.h"
+#include "std_msgs/String.h"
+
+ /**
+ * @brief Callback function to print chatter topic messages to the terminal
+ * @param msg Topic message
+ * @return None
+ */
+void chatterCallback(const std_msgs::String::ConstPtr& msg) {
+    ROS_INFO("Message read: [%s]", msg->data.c_str());
+}
+
+int main(int argc, char **argv) {
+    ros::init(argc, argv, "listener");
+
+    // Create node handle and subscriber to subscribe to chatter topic
+    ros::NodeHandle n;
+    ros::Subscriber sub = n.subscribe("chatter", 1000, chatterCallback);
+
+    ros::spin();
+
+    return 0;
+}
